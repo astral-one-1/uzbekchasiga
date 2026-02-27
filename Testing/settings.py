878fe8b45b72c9584866a6f814537dc927744306
astral-one-1 <xudoyberdirajabov222@gmail.com>
2026-02-27 14:32:51 +0500

    Set DEBUG to False for production readiness

--- Testing/settings.py
+++ Testing/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8@tsxp)3dz8133up58^ecxwqxaw!j+*)1sy5td_w6tyru#5(hb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["161.35.225.173"]  # yoki domeningizni ham qo'shing
 
