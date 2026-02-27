830abd26c91df80c4491a388932a1115b1022ce5
astral-one-1 <xudoyberdirajabov222@gmail.com>
2026-02-27 09:45:49 +0500

    Add static file serving to urlpatterns

--- Testing/urls.py
+++ Testing/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("TestApp.urls"))
-]
+]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
